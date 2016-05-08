0a1036c5baf168fedb4e0b4e9d14b93849fdcde6
Simon Leinen <simon.leinen@gmail.com>
2016-05-08 14:03:11 +0200

    Extract existence test to simplify recursive core

--- B.lisp
+++ B.lisp
@@ -22,10 +22,10 @@
   (ash 1 (max (- B 2) 0)))
 
 (defun slides (B M)
-  (cond ((= B 1)
-	 (ecase M (1 (list 0))))
-	((> M (maxpaths B))
-	 nil)
-	(t
-	 (cons (if (< M (maxpaths B)) (ash M 1) (- (* M 2) 1))
-	       (slides (1- B) (maxpaths (1- B)))))))
+  (and (<= M (maxpaths B)) (slides-1 B M)))
+
+(defun slides-1 (B M)
+  (if (= B 1)
+      (list 0)
+      (cons (if (< M (maxpaths B)) (ash M 1) (- (* M 2) 1))
+	    (slides-1 (1- B) (maxpaths (1- B))))))
